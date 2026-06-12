704b730749e1994bba0e29e68c5222d3a396813e
adrianisaguirre23-sudo <adrianisaguirre23@gmail.com>
2026-06-11 21:46:45 -0700

    fix MongoStore

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ process.on('uncaughtException', err => {
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 const methodOverride = require('method-override');
 const path = require('path');
 
